48cbc6ffe58388b24abe74833f05d226de5401d0
Tamir <tamirmoa123@gmail.com>
2017-10-11 19:49:34 +0200

    * Added Adir's files to project. * Wrote Jury.cpp. * CourtHouse.cpp:  * No need to initiate judges array for zero size.  * Added delete for all string setters before copying.  * Threw exception on Ctor when number of rooms are negative.  * Fixed setCity exception message.  * deleted the array of judges in dtor.  * set nullptr in all pointers in Ctor.  * addJudge check was the other way around.  * Fixed braces to be like C and not Java.  * Added missing "throw" in addJudge method.  * fixed addJudge logic.  * deleted old array in addJudge.  * Fixed compilation problems in getJudgeByName.  * Removed all sizeOf use (no need).  * Checked if there is no judge with the given name in getJudgeByName.  * Removed all use of getters in all methods of the class.  * Fixed compilation errors in addCourtRoom.  * Fixed exception message in addCourtRoom method.  * Fixed Operator[]  * Fixed compliation errors in operator << * CourtHouse.h: Chaged exceptions to raise const char*.  * Set operator= private.  * Changed getters and setters of strings to work with const char*.  * Removed getAllJudges() method (not safe). * CourtWorker.cpp: Fixed exception text in setStartYear. * Jury.h:  * Moved operator= to private.  * Removed getAllJuryMembers method (no need).  * addJuryMember getting pointer cause there is no copy ctor.  * Removed operators +=, -=.  * Removed toOs (no need) and added operator<< instead.  * Added operator==. * Party.cpp:  * Checked if name is not null in setter.  * Added operator!=. * Party.h:  * Added operator!=.  * Threw an exception is setName. * Person.cpp:  * Throwing exception in setName if name is null. * Person.h: Set Person an abstruct class, Throwing exception in setName. * TmUtilities: Added diff method. * Trial.cpp: Fixed compilation errors, trowing exceptions in setStart/endTime when not suitable to the other. * Trial.h: no need to return bool in setters if throwing an exception.

---
+++ Court/Jury.cpp
@@ -0,0 +1,154 @@
+#include "Jury.h"
+
+Jury::Jury(JuryMember** allJuryMembers, int maxNumOfMembers) throw(const char*) :
+allJuryMembers(nullptr)
+{
+	if(this->maxNumOfMembers > 0)
+	{
+		this->maxNumOfMembers = maxNumOfMembers;
+		
+		setJuryMembers(allJuryMembers);
+	}
+	else
+	{
+		throw("The number of maximum jury members has to be larger than 0!");
+	}
+}// maxNumOfMembers must be a non-zero positive, "allJuryMembers" cannot be a nullptr
+
+Jury::~Jury()
+{
+	for(int i = 0; i < numOfMembers; i++)
+	{
+		delete allJuryMembers[i];
+	}
+
+	delete []allJuryMembers;
+}
+
+void Jury::setJuryMembers(JuryMember** allJuryMembers) throw(const char*)
+{
+	if(allJuryMembers != nullptr)
+	{
+		this->allJuryMembers = allJuryMembers;
+	}
+	else
+	{
+		throw("Jury members array must not be null!");
+	}
+}
+
+inline int Jury::getNumOfJuryMembers() const
+{
+	return numOfMembers;
+}
+
+void Jury::addJuryMember(JuryMember* juryMember) throw(const char*)
+{
+	if(juryMember == nullptr)
+	{
+		throw("Adding null juryMember not allowed (in Jury)");
+	}
+	else
+	{
+		if(this->numOfMembers + 1 <= maxNumOfMembers)
+		{
+			this->allJuryMembers[this->numOfMembers] = juryMember;
+			++this->numOfMembers;
+		}
+		else
+		{
+			throw("Adding juryMember not allowed (maxNumOfMembers exceeded, in Jury)");
+		}
+	}
+}//throws a message if "juryMember" is a nullptr or maxNumOfMembers already exceeded
+
+bool Jury::removeJuryMember(int id) throw(const char*)
+{
+	bool isRemoved = false;
+
+	for(int i = 0; i < numOfMembers; i++)
+	{
+		if( allJuryMembers[i]->getId() == id)
+		{
+			delete allJuryMembers[i];
+			
+			// Shifting all members one place left
+			for(int j = i; j < numOfMembers - 1; j ++)
+			{
+				allJuryMembers[i] = allJuryMembers[i + 1];
+			}
+			
+			--numOfMembers;
+
+			isRemoved = true;
+		}
+	}
+
+	return isRemoved;
+}
+
+const JuryMember& Jury::operator[](int index) const throw(const char*)
+{
+	if(index >=0 && index < numOfMembers)
+	{
+		return *allJuryMembers[index];
+	}
+	else
+	{
+		throw("Array index out of range (inside jury, operator [])");
+	}
+}
+
+JuryMember& Jury::operator[](int index) throw(const char*)
+{
+	if(index >=0 && index < numOfMembers)
+	{
+		return *allJuryMembers[index];
+	}
+	else
+	{
+		throw("Array index out of range (inside jury, operator [])");
+	}
+}
+
+bool Jury::operator==(const Jury& other) const
+{
+	bool isEqual = true;
+
+	if(this->maxNumOfMembers == other.maxNumOfMembers &&
+		this->numOfMembers == numOfMembers)
+	{
+		for(int i = 0; i < numOfMembers; i++)
+		{
+			if( allJuryMembers[i] != other.allJuryMembers[i])
+			{
+				isEqual = false;
+			}
+		}
+	}
+	else
+	{
+		isEqual = false;
+	}
+
+	return isEqual;
+}
+
+ostream& operator<<(ostream& os, const Jury& jury)
+{
+	os << "Jury: ";
+	
+	if(jury.numOfMembers == 0)
+	{
+		os << "No members";
+	}
+	else
+	{
+		for(int i = 0; i < jury.numOfMembers; i++)
+		{
+			os << jury.allJuryMembers[i] << endl;
+		}
+	}
+
+	return os;
+}
